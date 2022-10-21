2ba2f993b86ff2270f4b30865cda1cb0cb32671d
Evelyn dos Santos <vrevyn@gmail.com>
2022-10-21 17:47:48 -0300

    :construction: Botoes de servidor  :construction:

---
+++ src/components/ServerButton/index.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+import Logo from "../../assets/foto.jpg";
+
+import {Button } from "./styles";
+
+export interface Props{
+    selected?: boolean;
+    isHome?: boolean;
+    hasNotification?: boolean;
+    mentions?: number;
+}
+
+
+const ServerButton: React.FC<Props> = ({
+    selected,
+    isHome,
+    hasNotification,
+    mentions,
+}) => {
+
+    return(
+        <Button
+        isHome = {isHome}
+        hasNotification = {hasNotification}
+        mentions = {mentions}
+        className = {selected ? 'active' : ''}
+        >
+           {isHome && <img src={Logo} alt="Logo" />} 
+        </Button>
+    )
+
+};
+
+export default ServerButton;
--- src/components/ServerList/index.tsx
+++ src/components/ServerList/index.tsx
@@ -5,7 +5,18 @@ import { Container, Separator } from './styles';
 const ServerList: React.FC = () => {
     return(
         <Container>
+            <ServerButton isHome/>
             <Separator/>
+            <ServerButton hasNotification/>
+            <ServerButton hasMention/>
+            <ServerButton/>
+            <ServerButton hasNotification/>
+            <ServerButton hasNotification/>
+            <ServerButton/>
+            <ServerButton hasMention/>
+            <ServerButton/>
+            <ServerButton/>
+            <ServerButton/>
         </Container>
     );
 }
